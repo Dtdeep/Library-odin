afbf558e0639f66b8d45798cf2e1fccd5cac504a
Dtdeep <dumagandesthiny@gmail.com>
2026-05-12 17:13:26 +0800

    Add Delete and Toggle Button for each card

--- javascript.js
+++ javascript.js
@@ -1,11 +1,9 @@
 const mainContainer = document.querySelector(".main-container")
 const booksContainer = document.querySelector("#books-container");
-const titleInput = document.querySelector("#title-input")
-const authorInput = document.querySelector("#author-input")
-const pagesInput = document.querySelector("#pages-input")
-const readInput = document.querySelector("#read-input")
-
-
+const titleInput = document.querySelector("#title-input");
+const authorInput = document.querySelector("#author-input");
+const pagesInput = document.querySelector("#pages-input");
+const readInput = document.querySelector("#read-input");
 
 const myLibrary = [];
 
@@ -34,15 +32,18 @@ const renderCards = () => {
                 const pPages = document.createElement("p");
                 const pRead = document.createElement("p");
                 const pId = document.createElement("p");
+                const readToggle = document.createElement("button");
 
                 bookCard.dataset.id = item.id.toString();
                 closeBtn.textContent = "x"
-                closeBtn.setAttribute("id", "close-btn")
-                pId.textContent = `Book ID: ${item.id}`
+                closeBtn.setAttribute("id", "close-btn");
+                readToggle.setAttribute("id", "toggle-btn");
+                pId.textContent = `${item.id}`;
                 pTitle.textContent = `Title: ${item.title}`;
-                pAuthor.textContent = `Author: ${item.author}`;
-                pPages.textContent = `Pages: ${item.pages}`;
-                pRead.textContent = `Read: ${item.read}`;
+                pAuthor.textContent = `By ${item.author}`;
+                pPages.textContent = `${item.pages} Pages`;
+                pRead.textContent = `Read Status: ${item.read}`;
+                readToggle.textContent = `Toggle`;
                 
                 booksContainer.appendChild(bookCard);
                 bookCard.appendChild(closeBtn);
@@ -51,25 +52,59 @@ const renderCards = () => {
                 bookCard.appendChild(pAuthor);
                 bookCard.appendChild(pPages);
                 bookCard.appendChild(pRead);
-                console.log(`ïts working?!`);
+                bookCard.appendChild(readToggle);
+                console.log(`Rendering Success`);
         })
+};
+
+
+Book.prototype.toggleReadStatus = function(status){
+    let read = status;
+    if(read === true){
+        read = false;
+        this.read = `Not Read Yet`
+    }else{
+        read = true;
+        this.read = `Read Already`; 
+    }
+    renderCards();
 }
 
 mainContainer.addEventListener('click' , (event)=>{
     let target = event.target;
-    
+
     switch(target.id){
         case "submit-btn":
-            const titleText = titleInput.textContent;
-            const authorText = authorInput.textContent;
-            const pagesText = pagesInput.textContent;
-            const readText = readInput.textContent;
+            const titleText = titleInput.value;
+            const authorText = authorInput.value;
+            const pagesText = pagesInput.value;
+            const readText = readInput.value;
             addBookToLibrary(titleText, authorText, pagesText, readText);
+            console.log("Creation of book object Complete");
             renderCards();
             event.preventDefault();
             break;
         case "close-btn":
-            console.log("closee!!!!")
+            const deleteIndex = myLibrary.findIndex((item)=>{
+                return item.id === target.parentNode.dataset.id;
+            });
+            myLibrary.splice(deleteIndex, 1);
+            renderCards();
+            console.log(myLibrary);
+            console.log("Deletion of book object complete");
+            break;
+        case "toggle-btn":
+            let status;
+            const findIndex = myLibrary.findIndex((item)=>{
+                return item.id === target.parentNode.dataset.id;
+            });
+
+            if(myLibrary[findIndex].read === "Read Already"){
+                status = true;
+            }else if (myLibrary[findIndex].read === "Not Read Yet"){
+                status = false;
+            }
+            myLibrary[findIndex].toggleReadStatus(status);
             break;
     }
 })
